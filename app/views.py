78093fe8175073b6448670022d0a760eae11576e
pinlil <iran.pinsocial@gmail.com>
2018-12-15 23:08:58 +0330

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+
+class Index(TemplateView):
+    template_name = 'app/index.html'
+
+
+
+
+# Create your views here.
